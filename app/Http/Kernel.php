e4ed87c5fe59930188fd4084d5abe6a0b39c89b7
ปราชญ์ หาญ <prachhankiet@gmail.com>
2024-06-21 18:14:43 +0700

    codes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,3 +2,20 @@ protected $routeMiddleware = [
     // Other middleware
     'role' => \App\Http\Middleware\CheckRole::class,\App\Http\Middleware\TrustAllProxies::class,
 ];
+
+protected $middlewareGroups = [
+    'web' => [
+        \App\Http\Middleware\EncryptCookies::class,
+        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        \Illuminate\Session\Middleware\StartSession::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \App\Http\Middleware\VerifyCsrfToken::class,
+        \Illuminate\Routing\Middleware\SubstituteBindings::class,
+    ],
+
+    'api' => [
+        'throttle:api',
+        \Illuminate\Routing\Middleware\SubstituteBindings::class,
+    ],
+];
+
